e3ba0c4d311310e785c2482ad67e45fd92116fa1
mlhiter <3076438032@qq.com>
2024-10-03 11:39:25 +0800

    feat: auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from '@/components/ui/button'
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
-  return <Button variant={'blue'}>Click me</Button>
-}
